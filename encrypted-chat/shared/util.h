20f3e95bc1788d4fda9b7ce48f6826c9830fda34
SALLY PARK <sallypark73@hotmail.com>
2024-05-14 01:18:37 -0700

    added header and ops to util

--- encrypted-chat/shared/util.h
+++ encrypted-chat/shared/util.h
@@ -1,3 +1,26 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 #define SERVER_PORT 12345
+
+//------------------------------------------------------------------------------
+// packets/requests/responses
+//------------------------------------------------------------------------------
+enum Operation {
+    OPERATION_SEND_MSG,
+    OPERATION_UPDATE_CHAT,
+    // OPERATION_CREATE_SESSION,
+    // OPERATION_JOIN_SESSION,
+};
+
+string getOperationName(Operation op);
+// example use: Operation op = OPERATION_SEND_MSG; if (op == OPERATION_SEND_MSG) {...}
+
+struct header {
+    int user_id;
+    Operation op_id;
+    int data_len;
+    bool result; // only used for response packet
+};
