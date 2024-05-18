6ee97af9ac37b7420c053c5e0220f04b73d665ea
Edwin Chow <edwinchow1110@gmail.com>
2024-05-17 17:46:53 -0700

    deleted unneccessary comments

--- encrypted-chat/server/server.cpp
+++ encrypted-chat/server/server.cpp
@@ -116,7 +116,7 @@ int main() {
 		}
 
 		// message from client, figure out which client is attempting to send a message
-		for (int i = 0; i < max_fds + 1; i++) {
+		for (int i = 0; i <= max_fds; i++) {
 			if (FD_ISSET(i, &read_fds) != 0) {
 				std::cout << "server.cpp::main(): client on fd " << i << " has sent a message" << std::endl;
 
@@ -146,84 +146,5 @@ int main() {
 				send(i, buffer, sizeof(buffer), 0);
 			}
 		}
-
-			// check if activity is on the server's socket fd = connection request
-			// handle requests
-			// connection request handler
-				// we accept the client here and add the clients fd to the descriptor set
-				// accept
-
-			// else if (...)
-				// messages... POO!
-
-			// poohaps handle disconnection stuffs peehead
 	}
 }
-
-
-// int main()
-// {
-// 	//------------------------------------------------------------------------------
-// 	// Initialize socket and block until a client connects
-// 	//------------------------------------------------------------------------------
-
-// 	// Create TCP socket
-// 	int server_socketfd = socket(AF_INET, SOCK_STREAM, 0);
-
-// 	if (server_socketfd == -1)
-// 	{
-// 		std::cerr << "server.cpp::main(): socket() threw an error with code: " << strerror(errno) << std::endl;
-
-// 		throw std::runtime_error("server.cpp::main(): Unable to allocate file descriptor when attempting to initialize socket");
-// 	}
-// 	else {
-// 		std::cout << "server.cpp::main(): Connection established, file descriptor: " << server_socketfd << std::endl;
-// 	}
-
-// 	sockaddr_in serverAddress;
-// 	serverAddress.sin_family = AF_INET;
-// 	serverAddress.sin_port = htons(SERVER_PORT);
-// 	serverAddress.sin_addr.s_addr = INADDR_ANY;
-// 	// cout << "SERVER_PORT: " << SERVER_PORT << endl;
-
-// 	bind(server_socketfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-	
-
-// 	// Create a connection queue and initialize readfds to handle input from server_sockfd.
-// 	listen(server_socketfd, 5);
-
-// 	// Creates a new socket and returns a unique FD to communicate with an individual client
-// 	std::cout << "server.cpp::main(): Waiting for client connection... " << std::endl;
-
-// 	int serverAddressLen = sizeof(struct sockaddr_in);
-// 	// int clientSocket = accept(server_socketfd, nullptr, nullptr);
-// 	int clientSocket = accept(server_socketfd, (struct sockaddr *)&serverAddress,(socklen_t *)&serverAddressLen);
-// 	if (clientSocket < 0) {
-// 		perror("Accept connection");
-// 	}
-// 	std::cout << "server.cpp::main(): client accepted" << std::endl;
-
-// 	//--------------------------------------
-// 	// Receive data from client and echos
-// 	//--------------------------------------
-// 	while (1) {
-//         // cout << "server.cpp::main(): while" << endl;
-// 		char buffer[1024] = {0};
-// 		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-// 		if (bytesReceived == 0) {
-// 			std::cout << "server.cpp::main(): Client disconnected!" << std::endl;
-// 			break;
-// 		}
-// 		if (bytesReceived < 0) {
-// 			// Received no data (client may not be connected yet), try again
-// 			continue;
-// 		}
-
-// 		// Successfully received data from client
-// 		std::cout << "server.cpp::main(): Client sent: " << buffer << std::endl;
-// 		// Echo data back to client
-// 		send(clientSocket, buffer, sizeof(buffer), 0);
-// 	}
-
-// 	close(server_socketfd);
-// }
