8788e4c0a2eaef3584cdfd5f43a8c9b40c0a86df
SALLY PARK <sallypark73@hotmail.com>
2024-05-21 22:48:30 -0700

    stopid

--- encrypted-chat/shared/packet_protocol.cpp
+++ encrypted-chat/shared/packet_protocol.cpp
@@ -18,7 +18,7 @@ PacketProtocol::PacketProtocol() {
  * @param user_id Sender's user ID.
  * @param op_id Operation ID.
  * @param result Result code in the response packet.
- * @param data_len Length of the attached data
+ * @param data_len Length of the attached data.
  * @param data The attached data.
 */
 PacketProtocol::PacketProtocol(uint32_t user_id, uint8_t op_id, uint8_t result, uint16_t data_len, uint8_t *data) {
@@ -30,7 +30,7 @@ PacketProtocol::PacketProtocol(uint32_t user_id, uint8_t op_id, uint8_t result, 
  * @param user_id Sender's user ID.
  * @param op_id Operation ID.
  * @param result Result code in the response packet.
- * @param data_len Length of the attached data
+ * @param data_len Length of the attached data.
  * @param data The attached data.
 */
 void PacketProtocol::initPacket(uint32_t user_id, uint8_t op_id, uint8_t result, uint16_t data_len, uint8_t *data) {
@@ -44,50 +44,24 @@ void PacketProtocol::initPacket(uint32_t user_id, uint8_t op_id, uint8_t result,
 /**
  * used for parsing received packets; we use this instead of the setters since here we don't want to change the contents of the packet array, we only extract the fields from it.
  * we have already read in the buffer at this point and stored it in pkt_protocol->packet using read(pkt_protocol->packet, num_bytes) and then we call parsePacket() to extract the fields.
+ * user_id = 27 bits, op_id = 5 bits, result = 8 bits, data_len = 16 bits
 */
 void PacketProtocol::parsePacket() {
-    // uint32_t user_id; // 27 bits
-    // uint8_t op_id; // 5 bits
-    // uint8_t result; // 8 bits; only used for response
-    // uint16_t data_len; // use the full 16 bits to make the data offset easier. we send over the header size + data_len
-    // uint8_t *data;
-
-    // Sally
-    // user_id = 0x00000000 & ((uint32_t)packet[3] >> 5) & ((uint32_t)packet[2] << (8 - 5)) & ((uint32_t)packet[1] << (8 - 5 + 8)) & ((uint32_t)packet[0] << (8 - 5 + 16));
-    // op_id = packet[3] & 0b00011111;
-    // result = packet[4];
-    // // data_len = 0x0000 & ((uint16_t)packet[6] >> 4) & ((uint16_t)packet[5] << 4);
-    // data_len = 0x0000 & (uint16_t)packet[6] & ((uint16_t)packet[5] << 8);
-    // data = &packet[7];
-
-    // Ed
+    // TODO: merge with setPacket()?
     this->user_id = 0x00000000 | ((uint32_t)packet[3] >> 5) | ((uint32_t)packet[2] << (8 - 5)) | ((uint32_t)packet[1] << (8 - 5 + 8)) | ((uint32_t)packet[0] << (8 - 5 + 16));
     this->op_id = packet[3] & 0b00011111;
     this->result = packet[4];
     this->data_len = 0x0000 | (uint16_t)packet[6] | ((uint16_t)packet[5] << 8);
     this->data = &packet[7];
-
-    // cout << "packet_protocol.cpp::PacketProtocol::parsePacket(): Values parsed from packet: " << endl;
-    // cout << endl;
-    // cout << "user_id: " << static_cast<int>(user_id) << endl;
-    // cout << "op_id: " << static_cast<int>(op_id) << endl;
-    // cout << "result: " << static_cast<int>(result) << endl;
-    // cout << "data_len: " << static_cast<int>(data_len) << endl;
-    // if (data != nullptr) {
-    //     cout << "data: " << data << endl;
-    // } else {
-    //     cout << "data: NULL" << endl;
-    // }
-    // cout << endl;
 }
 
 void PacketProtocol::dumpPacket() {
     cout << "\n//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
     cout << "   PacketProtocol:\n"
-    << "      User ID: " << this->getUserId() << "\n" 
-    << "      Operation Code: " << static_cast<Operation>(this->getOpId()) << "\n" 
-    << "      Result Code: " << static_cast<ResultCodes>(this->getResult()) << "\n" 
-    << "      Data Length: " << this->getDataLen() << "\n" 
+    << "      User ID: " << this->getUserId() << "\n"
+    << "      Operation Code: " << static_cast<Operation>(this->getOpId()) << "\n"
+    << "      Result Code: " << static_cast<ResultCodes>(this->getResult()) << "\n"
+    << "      Data Length: " << this->getDataLen() << "\n"
     << "      Data: " << this->getData() << endl;
     // TODO: print data in a readable format (need a helper)
     cout << "//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n" << endl;
@@ -95,7 +69,9 @@ void PacketProtocol::dumpPacket() {
 
 /**
  * parses a message given a pointer to its first character, for a given number of bytes
- * returns the message as a string
+ * @param data_len ...
+ * @param data ...
+ * @return the message as a string
 */
 string PacketProtocol::parseData(uint16_t data_len, unsigned char *data) {
     string parsedString(reinterpret_cast<char*>(data), data_len);
